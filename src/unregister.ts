67a38b9415d65a913b9353ad6f00858175dbabe2
kp54 <kangpang65@gmail.com>
2022-03-19 16:32:34 +0900

    tweak error handling

--- src/unregister.ts
+++ src/unregister.ts
@@ -5,21 +5,29 @@ import config from './config';
 (async () => {
   const rest = new REST({ version: '9' }).setToken(config.discordToken);
 
-  try {
-    const resp = await rest.get(
-      Routes.applicationGuildCommands(config.clientId, config.guildId),
-    ) as APIApplicationCommand[];
+  const resp = await (async () => {
+    try {
+      return await rest.get(
+        Routes.applicationGuildCommands(config.clientId, config.guildId),
+      ) as APIApplicationCommand[];
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      return [];
+    }
+  })();
 
-    await resp
-      .filter((x) => x.application_id === config.clientId)
-      .map((x) => () => {
-        rest.delete(
+  await resp
+    .filter((x) => x.application_id === config.clientId)
+    .map((x) => async () => {
+      try {
+        await rest.delete(
           Routes.applicationGuildCommand(config.clientId, config.guildId, x.id),
         );
-      })
-      .reduce((acc, x) => acc.then(x), Promise.resolve());
-  } catch (error) {
-    // eslint-disable-next-line no-console
-    console.error(error);
-  }
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    })
+    .reduce((acc, x) => acc.then(x), Promise.resolve());
 })();
