4d9863b6f6a108d0f754353538d91e5237d16cd9
kp54 <kangpang65@gmail.com>
2023-01-28 01:51:42 +0900

    extend CommandContext

--- src/core/client.ts
+++ src/core/client.ts
@@ -2,13 +2,21 @@ import { Message } from 'discord.js';
 import { CommandContext } from './feature.js';
 
 export const createCommandContext = (message: Message): CommandContext => {
+  const author = {
+    id: message.author.id,
+    username: message.author.username,
+  };
+
   const reply = (text: string) => message.reply(text);
   const post = (text: string) => message.channel.send(text);
 
   return {
+    author,
     reply,
     post,
   };
 };
 
-export default {};
+export default {
+  createCommandContext,
+};
--- src/core/feature.ts
+++ src/core/feature.ts
@@ -1,4 +1,8 @@
 export type CommandContext = {
+  author: {
+    id: string;
+    username: string;
+  };
   reply: (message: string) => void;
   post: (message: string) => void;
 };
