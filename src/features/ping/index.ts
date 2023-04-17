7394bc6fa190f0d0e716ebfe7c26f762746b6cd4
kp54 <kangpang65@gmail.com>
2023-04-17 20:11:44 +0900

    ping: add responses

--- src/features/ping/index.ts
+++ src/features/ping/index.ts
@@ -1,11 +1,30 @@
 import { defineFeature } from '../../core/feature.js';
 
-export default defineFeature(({ config }) => ({
-  matcher: new RegExp(`^${config.core.prefix}ping$`),
+const pingPong = new Map([
+  ['ping', 'pong!'], // ping-pong
+  ['pong', 'ping!'], // vice versa
+  ['hong', 'kong!'], // 香港
+  ['kang', 'pang!'], // it's me, @kp54
+]);
 
-  onCommand: async (ctx) => {
-    if (ctx.type === 'CHANNEL') {
-      await ctx.reply('pong!');
+const keys = Array.from(pingPong)
+  .map(([key, _value]) => key)
+  .join('|');
+
+export default defineFeature(({ config }) => ({
+  matcher: new RegExp(`^${config.core.prefix}(${keys})$`),
+
+  onCommand: async (ctx, match) => {
+    if (ctx.type !== 'CHANNEL') {
+      return;
     }
+
+    const ping = match[0].slice(config.core.prefix.length);
+    const pong = pingPong.get(ping);
+    if (pong === undefined) {
+      return;
+    }
+
+    await ctx.reply(pong);
   },
 }));
