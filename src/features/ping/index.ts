ac031ba78c47c491e49f4230eb53cf2f57d6607f
kp54 <kangpang65@gmail.com>
2023-06-18 22:42:42 +0900

    Revert "ping: add responses"
    This reverts commit 7394bc6fa190f0d0e716ebfe7c26f762746b6cd4.

--- src/features/ping/index.ts
+++ src/features/ping/index.ts
@@ -1,30 +1,11 @@
 import { defineFeature } from '../../core/feature.js';
 
-const pingPong = new Map([
-  ['ping', 'pong!'], // ping-pong
-  ['pong', 'ping!'], // vice versa
-  ['hong', 'kong!'], // 香港
-  ['kang', 'pang!'], // it's me, @kp54
-]);
-
-const keys = Array.from(pingPong)
-  .map(([key, _value]) => key)
-  .join('|');
-
 export default defineFeature(({ config }) => ({
-  matcher: new RegExp(`^${config.core.prefix}(${keys})$`),
+  matcher: new RegExp(`^${config.core.prefix}ping$`),
 
-  onCommand: async (ctx, command) => {
-    if (ctx.type !== 'CHANNEL') {
-      return;
+  onCommand: async (ctx) => {
+    if (ctx.type === 'CHANNEL') {
+      await ctx.reply('pong!');
     }
-
-    const ping = command.match[0].slice(config.core.prefix.length);
-    const pong = pingPong.get(ping);
-    if (pong === undefined) {
-      return;
-    }
-
-    await ctx.reply(pong);
   },
 }));
